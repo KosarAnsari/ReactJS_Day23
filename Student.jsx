66d9ef7ffd604108f4339af908fbe1dfe2c2d21f
KosarAnsari <kosaransari55@gmail.com>
2025-08-27 14:37:31 +0530

    Create Student.jsx

---
+++ Student.jsx
@@ -0,0 +1,10 @@
+import Subject from './Subject';
+
+export default function Student() {
+  return (
+    <div style={{ backgroundColor: 'green', padding: '10px' }}>
+      <h2>Student</h2>
+      <Subject />
+    </div>
+  );
+}
